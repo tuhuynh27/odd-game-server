74ec524b31539301c73a5a81d9c0a26fd297e520
Tu Huynh <tu.huynh@linecorp.com>
2020-06-18 16:33:16 +0700

    Add room size

--- modules/room/room.schema.js
+++ modules/room/room.schema.js
@@ -4,7 +4,10 @@ const validateMiddleware = require('utils/handlers/validate.middleware')
 const createRoomSchema = Joi.object({
   name: Joi.string()
     .min(3)
-    .required()
+    .required(),
+  size: Joi.number()
+    .min(1)
+    .max(20)
 })
 
 const createRoomValidator = validateMiddleware(createRoomSchema)
