d9559fc68d121640b3763596d5226d4c7b3a863b
Tu Huynh <tu.huynh@linecorp.com>
2020-08-14 23:10:08 +0700

    add private chat websocket

--- modules/room/room.route.js
+++ modules/room/room.route.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const { createRoomValidator } = require('modules/room/room.schema')
-const { createRoom, getRooms } = require('modules/room/room.handler')
+const { createRoom, getRooms, getRoomChat } = require('modules/room/room.handler')
 
 router.route('/:country').get(getRooms)
 router.route('/:country').post(createRoomValidator, createRoom)
+router.route('/:slug/chat').get(getRoomChat)
 
 module.exports = router
